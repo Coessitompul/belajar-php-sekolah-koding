30a80f6b4db8689f2ef6c5c63b1ce4807f70d12e
Coes Sitompul <coessitompul99@gmail.com>
2020-09-24 15:13:18 +0700

    Video 3 : Membuat komentar dan Variabel

--- index.php
+++ index.php
@@ -8,9 +8,20 @@
 <body>
   
   <?php 
-    echo "<h1>Belajar php di sekolah koding</h1>";
-    echo "Halo <br>";
-    echo "Member";
+  /*
+    Beberapa baris 
+    komentar yang bisa anda tambahkan 
+    sendiri
+  */
+
+  // variabel pada php
+  // kita tidak boleh  menggunakan angka terlebih dahulu untuk membuat variabel, tetapi harus menggunakan huruf, kemudian jika perlu bisa anda gunakan angka untuk menambahkan variabel yang anda mulai dengan huruf kemudian bisa dilanjutan dengan angka jika diperlukan.
+    $nama = "Sekolah Koding";
+
+    echo "<h1>Belajar php di $nama</h1>";
+    echo "Halo". $nama ."<br>"; // titik (.) digunakan sebagai penghubung
+    echo "Hello Member";
+
   ?>
 
   Hai dunia !
